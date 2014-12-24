516ddce872a2a459dcdb868c80d6ed0cd7d097ad
George Chen <george.guo.chen@outlook.com>
2014-12-24 18:25:35 +0800

    Fixed urls, changed font color for blog title.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from blog import views
 
 urlpatterns = patterns(
     '',
-    url(r'^details/(?P<article_id>\d+)', views.show_article_details, name='show_article_details'),
+    url(r'^details/(?P<article_id>\d+)', views.show_article_details, name='blog_details'),
 )
