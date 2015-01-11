02b7751333173cd9e9736efbdd75642b65fe97f8
guo-chen <george.guo.chen@outlook.com>
2015-01-11 12:33:02 +0800

    Implemented 'Search by category and tag' functionatility

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+# from django.http.response import Http404
 # Generic class-based views
 from django.views.generic.base import TemplateView
 
@@ -8,7 +9,7 @@ from django.views.generic.base import TemplateView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from blog.models import Article
-from blog.models import Author
+from blog.models import Tag
 from blog.models import Category
 
 import datetime
@@ -57,6 +58,27 @@ class HomeView(TemplateView):
         context['archives'] = archives[:]
         return context
 
+    def get_query_set(self, category_name, tag_name):
+        archive_list = []
+        if category_name:
+            category = get_object_or_404(Category, name=category_name)
+            months_with_articles_in_category = category.article_set.all().datetimes('publish_time', 'month', 'DESC')
+            for month in months_with_articles_in_category:
+                articles_in_this_month = category.article_set.all().filter(publish_time__year=month.year, publish_time__month=month.month).order_by('-publish_time')
+                archive_list.append((month, articles_in_this_month))
+            # filter = category.name
+        elif tag_name:
+            tag = get_object_or_404(Tag, name=tag_name)
+            months_with_articles_with_tag = tag.article_set.all().datetimes('publish_time', 'month', 'DESC')
+            for month in months_with_articles_with_tag:
+                articles_in_this_month = tag.article_set.all().filter(publish_time__year=month.year, publish_time__month=month.month).order_by('-publish_time')
+                archive_list.append((month, articles_in_this_month))
+            # filter = tag.name
+        else:
+            archive_list = self.get_context_data()['archives']
+            # filter = None
+        return archive_list
+
     def get(self, request):
         article_list = Article.objects.all().order_by('-publish_time')
         paginator = Paginator(article_list, 5)  # Show 5 blogs in a page
@@ -86,6 +108,7 @@ class BlogDetailView(HomeView):
 
 class BlogsView(HomeView):
     template_name = "blog/blogs.html"
+    page_size = 10
 
 #     def get(self, request):
 #         article_list = Article.objects.all().order_by('-publish_time')
@@ -95,9 +118,11 @@ class BlogsView(HomeView):
 #         return render_to_response(self.template_name, context, context_instance=RequestContext(request))
 
     def get(self, request):
-        archive_list = self.get_context_data()['archives']
-        paginator = Paginator(archive_list, 10)
+        category_name = request.GET.get('category_name', '')
+        tag_name = request.GET.get('tag_name', '')
         page = request.GET.get('page')
+        archive_list = self.get_query_set(category_name, tag_name)
+        paginator = Paginator(archive_list, self.page_size)
         try:
             displayed_archive = paginator.page(page)
         except PageNotAnInteger:
